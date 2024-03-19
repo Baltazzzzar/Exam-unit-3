982d66d2959f6a2123444db0a8b3180f482324a9
Baltazzzzar <dariok@student.uia.no>
2024-03-19 22:55:46 +0100

    added ansi file to fix broken test file

--- test.cs
+++ test.cs
@@ -1,14 +1,16 @@
+using AnsiTools;
+
 public class Testing
 {
     public static void Test<T>(T expected, T actual, string description = "Test")
     {
         if (expected.Equals(actual))
         {
-            Console.WriteLine($"{Colors.Green} --PASSED--{description}.{Colors.Yellow}Expected: {expected}, Actual: {actual}{ANSICodes.Reset}");
+            Console.WriteLine($"{ANSICodes.Colors.Green} --PASSED--{description}.{ANSICodes.Colors.Yellow}Expected: {expected}, Actual: {actual}{ANSICodes.Reset}");
         }
         else
         {
-            Console.WriteLine($"{Colors.Red} --FAILED-- {description}.{Colors.Yellow} Expected: {expected}, {Colors.Red}Actual: {actual}{ANSICodes.Reset}");
+            Console.WriteLine($"{ANSICodes.Colors.Red} --FAILED-- {description}.{ANSICodes.Colors.Yellow} Expected: {expected}, {ANSICodes.Colors.Red}Actual: {actual}{ANSICodes.Reset}");
         }
     }
 }
