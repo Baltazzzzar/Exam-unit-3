666ad21a4fd382a2c549b5a5553b365c2b39e2c2
Baltazzzzar <dariok@student.uia.no>
2024-03-19 22:58:58 +0100

    Added tests for the current functions

--- tasks.cs
+++ tasks.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
+// Task 1
 
 double Square(double x)
 {
@@ -31,3 +32,15 @@ string Greet(string name)
 {
     return $"Good Day, {name}!";
 }
+
+
+
+
+// Testing
+
+Testing.Test(4, Square(2), "Square of 2");
+Testing.Test(9, Root(81), "Root of 81");
+Testing.Test(8, Cube(2), "Cube of 2");
+Testing.Test(Math.PI * 4, AreaOfCircle(2), "Area of Circle with radius 2");
+Testing.Test(50.8, InchToMm(2), "2 inch to mm");
+Testing.Test("Good Day, John!", Greet("John"), "Greet John");
